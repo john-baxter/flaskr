b13793b11d0c5d60443c324763b1788ebff1bfc0
john-baxter <john.g.baxter@googlemail.com>
2021-10-13 12:36:51 +0100

    Reinstate line 33 in  and correct syntax typo in blog.py.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -30,7 +30,7 @@ def create_app(test_config=None):
   app.register_blueprint(auth.bp)
 
   from . import blog
-  # app.register_blueprint(blog.bp)
+  app.register_blueprint(blog.bp)
   app.add_url_rule('/', endpoint='index')
 
   return app
